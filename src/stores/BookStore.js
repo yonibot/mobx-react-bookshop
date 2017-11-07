eaed8526dacc6fcb28cc88d5ab974342aba7f0b2
Yoni Weisbrod <git@yoni.sent.as>
2017-11-07 17:05:42 +0200

    add BookStore

---
+++ src/stores/BookStore.js
@@ -0,0 +1,45 @@
+import Book from "../models/Book";
+
+class BookStore {
+  @observable isLoading = false;
+  books = observable.map();
+
+  constructor(fetch) {
+    this.fetch = fetch;
+  }
+
+  @computed get sortedBooks() {
+    const books = this.books.values();
+    return books.sort((a,b) => {
+      return a.name > b.name
+        ? 1
+        : a.name == b.name
+          ? 0
+          : -1
+    })
+  }
+
+  @action loadBooks() {
+    this.loading = true;
+    this.fetch("books.json")
+      .then(json => {
+        this.updateBooks(json);
+        this.isLoading = false;
+      })
+      .catch(err => {
+        console.error("Failed to load books", err);
+      })
+  }
+
+  @action updateBooks(json) {
+    this.books.clear();
+    json.forEach(book => {
+      this.books.set(book.id, new Book(book))
+    })
+  }
+
+
+
+}
+
+export { BookStore as default };
