ffef2edb0b3979ba807a5f3fe2cec3bec05968da
Yoni Weisbrod <git@yoni.sent.as>
2017-11-07 17:10:30 +0200

    add bookstore to app

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import './index.css'
+import BookStore from './stores/BookStore';
+
+const fetcher = url => window.fetch(url).then(r=>r.json());
+const bookStore = new BookStore(fetcher);
 
 ReactDOM.render(
-  <App />,
+  <App bookStore={bookStore} />,
   document.getElementById('root')
 )
+
+// Hydrate initial state
+bookStore.loadBooks();
